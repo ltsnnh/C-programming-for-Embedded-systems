e40aca466b2b16529edff3a35f805c7ce4ad8a7a
ltsnnh <ltsnnh197@gmail.com>
2021-06-29 16:06:12 +0700

    add: first test program

---
+++ Variable-in-C/multidimensional-array/inc/sysincl.h
@@ -0,0 +1,11 @@
+#ifndef __SYSINCL_H__
+#define __SYSINCL_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void task_1(int *p);
+void task_2(int m, int n, int **a);
+void task_3(int m, int n, int k, int ***b);
+
+#endif
