58793f181a625ab0d0834ef0b90b833641752b96
ltsnnh <lt.anh197@gmail.com>
2021-07-14 22:57:33 +0700

    understand enum clearly

--- Variable-in-C/enum/src/main.c
+++ Variable-in-C/enum/src/main.c
@@ -2,12 +2,14 @@
 
 int main()
 {
-    enum week_day {SUNDAY, MONDAY, TUESDAY, WEDSDAY, THURSDAY, FRIDAY, SATURDAY} day;
-    enum week_day yesterday;
+    enum week_day {SUNDAY, MONDAY, TUESDAY, WEDSDAY, THURSDAY, FRIDAY, SATURDAY} day1 = 8, day2 = 4, day3 = 2;
+    enum week_day yesterday = 1, tomorrow = 2, the_next_day = 4;
 
-    printf("sizeof(enum week_day) = %ld = %ld = %ld\n", sizeof(day), sizeof(yesterday), sizeof(SUNDAY));
+    printf("sizeof(enum week_day) = %ld = %ld = %ld\n", sizeof(day1), sizeof(yesterday), sizeof(SUNDAY));
     printf("SUNDAY = %d\nMONDAY = %d\nTUESDAY = %d\nWEDSDAY = %d\nTHURSDAY = %d\nFRIDAY = %d\nSATURDAY = %d\n", \
            SUNDAY, MONDAY, TUESDAY, WEDSDAY, THURSDAY, FRIDAY, SATURDAY);
+    printf("day1 = %d\tday2 = %d\tday3 = %d\n", day1, day2, day3);
+    printf("yesterday = %d\ttomorrow = %d\tthe_next_day = %d\n", yesterday, tomorrow, the_next_day);
 
     return 0;
 }
