26018bbf2ed5ba8b896288691e036931ba15e78b
ltsnnh <lt.anh197@gmail.com>
2021-07-14 21:44:46 +0700

    add: compare string

---
+++ Variable-in-C/compare-string/src/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int main(void)
+{
+    char arr1[12] = "Ltsnnh";
+    //char arr2[12] = "Ltsnnh1";
+    char arr2[12] = {};
+    scanf("%s%*c", arr2);
+
+    if(strcmp(arr1,arr2)) // 0 -> Same | 1 -> Different
+    {
+        printf("Different string\n");
+    }
+    else
+    {
+        printf("Same string\n");
+    }
+
+    return 0;
+}
