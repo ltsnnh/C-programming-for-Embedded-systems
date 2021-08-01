c7485f3af9b300d8938114e9136f4a50fcfe5214
ltsnnh <lt.anh197@gmail.com>
2021-08-01 13:48:06 +0700

    rm asm-git, rm -std=c99

--- 0-Assignment/Assignment-git/merge/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("feature A");
-	printf("feature B");
-
-	return 0;
-}
--- 0-Assignment/Assignment-git/rebase/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("feature B");
-	printf("feature A");
-
-	return 0;
-}
